fa1102024fac1d087a1f764fa93ef6dcc7c98afb
DavidPinilla92 <d.pinilla@geekshubs.com>
2019-11-08 12:50:48 +0100

    a lot of changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { MovieCategoriesComponent } from './components/movie-categories/movie-categories.component';
 
 
 const routes: Routes = [
-  {path:"",component:MovieListComponent},
-  {path:"movies/:category",component:MovieCategoriesComponent}
+  {path: "", redirectTo: "discover", pathMatch:"full"},
+  {path:"discover", component:MovieListComponent},
+  {path:"movies/:category",component:MovieCategoriesComponent},
 ];
 
 @NgModule({
--- src/app/components/movie-categories/movie-categories.component.ts
+++ src/app/components/movie-categories/movie-categories.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { Movie } from 'src/app/models/movie.model';
 
 @Component({
   selector: 'app-movie-categories',
@@ -8,9 +10,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MovieCategoriesComponent implements OnInit  {
   category: string;
-  constructor(private route: ActivatedRoute) { }
+  peliculas:Array<Movie>;
+  constructor(private route: ActivatedRoute,//inyectamos el ActivatedRoute como dependencia para seguir los cambios de parámetros en la ruta
+     private movieService:MovieService //inyectamos el servicio movie para llamar al método getMoviesByCategory
+     ) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => this.category = params.get('category')) //me suscribo a cambios en los parámetros del router
+    this.route.paramMap.subscribe(params => {//me suscribo a cambios en los parámetros del router
+      this.category = params.get('category')//obtenemos el parámetro category
+      this.movieService.getMoviesByCategory(this.category) //lamamos al método del servicio movies para pedir las peliculas según la categoría
+      .subscribe(res=>{
+        this.peliculas=res['results'];// asignamos results a peliculas para poder acceder desde la plantilla HTML
+      })
+    })
   }
 }
